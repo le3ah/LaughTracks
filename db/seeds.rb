c8efedd6609b47e13ad4bb2fa0b39e6ae8c2b198
Leah Miller <lkmiller.87@gmail.com>
2018-11-28 08:50:57 -0700

    Adds seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
+require './app/models/comedian'
 
+Comedian.destroy_all
+
+Comedian.create(name: "Dana Carvey", age: 63, city: "Missoula")
+Comedian.create(name: "Jim Gaffigan", age: 52, city: "Elgin")
+Comedian.create(name: "Trevor Noah", age: 34, city: "Johannesburg")
+Comedian.create(name: "Dave Chappelle", age: 45, city: "DC")
+Comedian.create(name: "Norm MacDonald", age: 59, city: "Quebec City")
+Comedian.create(name: "Tracy Morgan", age: 50, city: "Brooklyn")
+Comedian.create(name: "Chris D'Elia", age: 38, city: "Montclair")
+Comedian.create(name: "Brian Regan", age: 60, city: "Miami")
+Comedian.create(name: "Steve Martin", age: 73, city: "Waco")
+Comedian.create(name: "Martin Short", age: 68, city: "Hamilton")
+Comedian.create(name: "Iliza Shlesinger", age: 35, city: "Manhattan")
+Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn")
