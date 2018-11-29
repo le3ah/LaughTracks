0ce746a932f9f49b4cd5d00b34964a1105fc00dc
Leah Miller <lkmiller.87@gmail.com>
2018-11-28 20:44:45 -0700

    Adds full tv specials information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_224826) do
+ActiveRecord::Schema.define(version: 2018_11_29_032409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_224826) do
     t.string "thumbnail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comedian_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,52 @@
 require './app/models/comedian'
+require './app/models/special'
 
 Comedian.destroy_all
 
-Comedian.create(name: "Dana Carvey", age: 63, city: "Missoula")
-comedian.specials.create(special_name: "Straight White Male, 60", run_time: 64, thumbnail: "./views/images/specials/straigh-white-male-60.jpg")
+comedian_1 = Comedian.create(name: "Dana Carvey", age: 63, city: "Missoula")
+comedian_1.specials.create(special_name: "Straight White Male, 60", run_time: 64, thumbnail: "./app/views/images/specials/straigh-white-male-60.jpg")
 
-Comedian.create(name: "Jim Gaffigan", age: 52, city: "Elgin")
-comedian.specials.create(special_name: "Cinco", run_time: 73, thumbnail: "./views/images/specials/Cinco.jpg")
-comedian.specials.create(special_name: "Obsessed", run_time: 79, thumbnail: "./views/images/specials/Obsessed.jpg")
-comedian.specials.create(special_name: "Jim Gaffigan: Mr. Universe", run_time: 74, thumbnail: "./views/images/specials/MrUniverse.jpg")
+comedian_2 = Comedian.create(name: "Jim Gaffigan", age: 52, city: "Elgin")
+comedian_2.specials.create(special_name: "Cinco", run_time: 73, thumbnail: "./app/views/images/specials/Cinco.jpg")
+comedian_2.specials.create(special_name: "Obsessed", run_time: 79, thumbnail: "./app/views/images/specials/Obsessed.jpg")
+comedian_2.specials.create(special_name: "Jim Gaffigan: Mr. Universe", run_time: 74, thumbnail: "./app/views/images/specials/MrUniverse.jpg")
 
-Comedian.create(name: "Trevor Noah", age: 34, city: "Johannesburg")
-comedian.specials.create(special_name: "Afraid of the Dark", run_time: 67, thumbnail: "./views/images/specials/afraid-of-dark.jpg")
-comedian.specials.create(special_name: "Son of Patricia", run_time: 63, thumbnail: "./views/images/specials/son-of-patricia.jpg")
+comedian_3 = Comedian.create(name: "Trevor Noah", age: 34, city: "Johannesburg")
+comedian_3.specials.create(special_name: "Afraid of the Dark", run_time: 67, thumbnail: "./app/views/images/specials/afraid-of-dark.jpg")
+comedian_3.specials.create(special_name: "Son of Patricia", run_time: 63, thumbnail: "./app/views/images/specials/son-of-patricia.jpg")
 
-Comedian.create(name: "Dave Chappelle", age: 45, city: "DC")
-comedian.specials.create(special_name: "The Age of Spin", run_time: 67, thumbnail: "./views/images/specials/age-of-spin.jpg")
-comedian.specials.create(special_name: "Deep in the Heart of Texas", run_time: 66, thumbnail: "./views/images/specials/deep-in-texas.jpg")
-comedian.specials.create(special_name: "Equanimity & the Bird Revelation", run_time: 112, thumbnail: "./views/images/specials/equanimity.jpg")
+comedian_4 = Comedian.create(name: "Dave Chappelle", age: 45, city: "DC")
+comedian_4.specials.create(special_name: "The Age of Spin", run_time: 67, thumbnail: "./app/views/images/specials/age-of-spin.jpg")
+comedian_4.specials.create(special_name: "Deep in the Heart of Texas", run_time: 66, thumbnail: "./app/views/images/specials/deep-in-texas.jpg")
+comedian_4.specials.create(special_name: "Equanimity & the Bird Revelation", run_time: 112, thumbnail: "./app/views/images/specials/equanimity.jpg")
 
-Comedian.create(name: "Norm MacDonald", age: 59, city: "Quebec City")
-comedian.specials.create(special_name: "Hitler's Dog, Gossip & Trickery", run_time: 61, thumbnail: "./views/images/specials/hitlers-dog.jpg")
+comedian_5 = Comedian.create(name: "Norm MacDonald", age: 59, city: "Quebec City")
+comedian_5.specials.create(special_name: "Hitler's Dog, Gossip & Trickery", run_time: 61, thumbnail: "./app/views/images/specials/hitlers-dog.jpg")
 
-Comedian.create(name: "Tracy Morgan", age: 50, city: "Brooklyn")
-comedian.specials.create(special_name: "Stayling Alive", run_time: 58, thumbnail: "./views/images/specials/staying-alive.jpg")
+comedian_6 = Comedian.create(name: "Tracy Morgan", age: 50, city: "Brooklyn")
+comedian_6.specials.create(special_name: "Stayling Alive", run_time: 58, thumbnail: "./app/views/images/specials/staying-alive.jpg")
 
-Comedian.create(name: "Chris D'Elia", age: 38, city: "Montclair")
-comedian.specials.create(special_name: "Incorrigible", run_time: 83, thumbnail: "./views/images/specials/incorrigible.jpg")
-comedian.specials.create(special_name: "Man on Fire", run_time: 65, thumbnail: "./views/images/specials/man-on-fire.jpg")
+comedian_7 = Comedian.create(name: "Chris D'Elia", age: 38, city: "Montclair")
+comedian_7.specials.create(special_name: "Incorrigible", run_time: 83, thumbnail: "./app/views/images/specials/incorrigible.jpg")
+comedian_7.specials.create(special_name: "Man on Fire", run_time: 65, thumbnail: "./app/views/images/specials/man-on-fire.jpg")
 
-Comedian.create(name: "Brian Regan", age: 60, city: "Miami")
-comedian.specials.create(special_name: "Nunchucks and Flamethrowers", run_time: 61, thumbnail: "./views/images/specials/nunchucks.jpg")
+comedian_8 = Comedian.create(name: "Brian Regan", age: 60, city: "Miami")
+comedian_8.specials.create(special_name: "Nunchucks and Flamethrowers", run_time: 61, thumbnail: "./app/views/images/specials/nunchucks.jpg")
 
-Comedian.create(name: "Steve Martin", age: 73, city: "Waco")
-comedian.specials.create(special_name: "An Evening You Will Forget for the Rest of Your Life", run_time: 73, thumbnail: "./views/images/specials/an-evening-you-will-forget.jpg")
-comedian.specials.create(special_name: "The 75th Annual Academy Awards", run_time: 210, thumbnail: "./views/images/specials/75th-academy.JPG")
-comedian.specials.create(special_name: "The 73rd Annual Academy Awards", run_time: 203, thumbnail: "./views/images/specials/73rd-academy.jpg")
-comedian.specials.create(special_name: "Steve Martin: A Wild and Crazy Guy", run_time: 60, thumbnail: "./views/images/specials/wild-and-crazy-guy.jpg")
+comedian_9 = Comedian.create(name: "Steve Martin", age: 73, city: "Waco")
+comedian_9.specials.create(special_name: "An Evening You Will Forget for the Rest of Your Life", run_time: 73, thumbnail: "./app/views/images/specials/an-evening-you-will-forget.jpg")
+comedian_9.specials.create(special_name: "The 75th Annual Academy Awards", run_time: 210, thumbnail: "./app/views/images/specials/75th-academy.JPG")
+comedian_9.specials.create(special_name: "The 73rd Annual Academy Awards", run_time: 203, thumbnail: "./app/views/images/specials/73rd-academy.jpg")
+comedian_9.specials.create(special_name: "Steve Martin: A Wild and Crazy Guy", run_time: 60, thumbnail: "./app/views/images/specials/wild-and-crazy-guy.jpg")
 
-Comedian.create(name: "Martin Short", age: 68, city: "Hamilton")
-comedian.specials.create(special_name: "An Evening You Will Forget for the Rest of Your Life", run_time: 73, thumbnail: "./views/images/specials/an-evening-you-will-forget.jpg")
-comedian.specials.create(special_name: "The Earth Day Special", run_time: 120, thumbnail: "./views/images/specials/earth-day.jpg")
+comedian_10 = Comedian.create(name: "Martin Short", age: 68, city: "Hamilton")
+comedian_10.specials.create(special_name: "An Evening You Will Forget for the Rest of Your Life", run_time: 73, thumbnail: "./app/views/images/specials/an-evening-you-will-forget.jpg")
+comedian_10.specials.create(special_name: "The Earth Day Special", run_time: 120, thumbnail: "./app/views/images/specials/earth-day.jpg")
 
-Comedian.create(name: "Iliza Shlesinger", age: 35, city: "Manhattan")
-comedian.specials.create(special_name: "War Paint", run_time: 203, thumbnail: "./views/images/specials/war-paint.jpg")
-comedian.specials.create(special_name: "Freezing Hot", run_time: 203, thumbnail: "./views/images/specials/freezing-hot.jpg")
-comedian.specials.create(special_name: "Elder Millennial", run_time: 203, thumbnail: "./views/images/specials/elder-millennial.jpg")
+comedian_11 = Comedian.create(name: "Iliza Shlesinger", age: 35, city: "Manhattan")
+comedian_11.specials.create(special_name: "War Paint", run_time: 75, thumbnail: "./app/views/images/specials/war-paint.jpg")
+comedian_11.specials.create(special_name: "Freezing Hot", run_time: 71, thumbnail: "./app/views/images/specials/freezing-hot.jpg")
+comedian_11.specials.create(special_name: "Elder Millennial", run_time: 72, thumbnail: "./app/views/images/specials/elder-millennial.jpg")
 
-Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn")
-comedian.specials.create(special_name: "100% Fresh", run_time: 203, thumbnail: "./views/images/specials/100-fresh.jpg")
+comedian_12 = Comedian.create(name: "Adam Sandler", age: 52, city: "Brooklyn")
+comedian_12.specials.create(special_name: "100% Fresh", run_time: 73, thumbnail: "./app/views/images/specials/100-fresh.jpg")
